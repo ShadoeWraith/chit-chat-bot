5088055acb5e45296a640bbd4c128d07327577e1
Shaun Kiszonas <shadoewraith99@gmail.com>
2025-05-06 01:17:23 -0700

    added info command

---
+++ commands/info.js
@@ -0,0 +1,44 @@
+import { SlashCommandBuilder, EmbedBuilder, MessageFlags } from 'discord.js';
+import { dbSync } from '../utils/dbsync.js';
+import { format } from 'date-fns';
+
+export const data = new SlashCommandBuilder()
+    .setName('info')
+    .setDescription('Displays info about yourself.')
+    .addUserOption((option) => option.setName('user').setDescription('The user you would like to view').setRequired(true));
+
+export async function execute(interaction) {
+    dbSync(interaction.guildId);
+
+    const targetUser = interaction.options.getUser('user');
+
+    const guild = interaction.guild;
+    const member = await guild.members.fetch(targetUser.id);
+
+    const userRoles = [];
+    member.roles.cache.map((role) => {
+        if (role.name !== '@everyone') userRoles.push(`<@&${role.id}>`);
+    });
+
+    console.log(userRoles);
+
+    const createdAt = format(member.user.createdAt, 'MM/dd/yyyy');
+    const joinedAt = format(member.joinedAt, 'MM/dd/yyyy');
+
+    const embed = new EmbedBuilder()
+        .setColor(0x00aaaaa)
+        .setTitle(`${member.user.globalName ? member.user.globalName : member.user.username}'s Information`)
+        .setThumbnail(member.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+        .setDescription('Displaying pointless information just because I was bored and wanted to keep working on the bot.')
+        .addFields(
+            { name: 'Display Name', value: member.user.globalName ? member.user.globalName : 'No Display Name' },
+            { name: 'Username', value: member.user.username },
+            { name: 'ID', value: member.user.id },
+            { name: 'Account Created', value: createdAt },
+            { name: 'Joined Server', value: joinedAt },
+            { name: 'Roles', value: userRoles.length !== 0 ? userRoles.join(' | ') : 'No Roles' }
+        )
+        .setTimestamp();
+
+    interaction.reply({ embeds: [embed], flags: MessageFlags.Ephemeral });
+}
